61aeeebdab87bcb96286a8988963b91bbc134bfb
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2012-10-18 21:46:16 +0100

    Changes coding style to Eclipse default

--- bfs-example/AdjListNode.java
+++ bfs-example/AdjListNode.java
@@ -1,25 +1,24 @@
-
 /**
- class to represent an entry in the adjacency list of a vertex
- in a graph
+ * class to represent an entry in the adjacency list of a vertex in a graph
  */
 public class AdjListNode {
 
-    private int vertexNumber;
-    // could be other fields, for example representing
-    // properties of the edge - weight, capacity, ...
-    
-    /* creates a new instance */
-    public AdjListNode(int n){
-        vertexNumber = n;
-    }
-    
-    public int getVertexNumber(){
-        return vertexNumber;
-    }
-    
-    public void setVertexNumber(int n){
-        vertexNumber = n;
-    }
-    
+	private int vertexNumber;
+
+	// could be other fields, for example representing
+	// properties of the edge - weight, capacity, ...
+
+	/* creates a new instance */
+	public AdjListNode(int n) {
+		vertexNumber = n;
+	}
+
+	public int getVertexNumber() {
+		return vertexNumber;
+	}
+
+	public void setVertexNumber(int n) {
+		vertexNumber = n;
+	}
+
 }
