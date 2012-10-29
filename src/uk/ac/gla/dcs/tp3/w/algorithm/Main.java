c6105af3392b88157f2bed487938010c9a62f433
Gordon Reid <gordon@gordon-ubuntu.(none)>
2012-10-29 17:19:07 +0000

    Reorganisation of class structure

---
+++ src/uk/ac/gla/dcs/tp3/w/algorithm/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package uk.ac.gla.dcs.tp3.w.algorithm;
+
+/**
+ * @author gordon
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
