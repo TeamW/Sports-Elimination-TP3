e3bd12f3134f0c53f1a78b4f1bc4665710422ece
gordon1992 <gordon.reid1992@hotmail.co.uk>
2012-11-01 18:29:38 +0000

    Creates discussed structure of shared objects

---
+++ src/uk/ac/gla/dcs/tp3/w/league/League.java
@@ -0,0 +1,24 @@
+package uk.ac.gla.dcs.tp3.w.league;
+
+public class League {
+
+	private Team[] teams;
+	private Match[] fixtures;
+
+	public Team[] getTeams() {
+		return teams;
+	}
+
+	public void setTeams(Team[] teams) {
+		this.teams = teams;
+	}
+
+	public Match[] getFixtures() {
+		return fixtures;
+	}
+
+	public void setFixtures(Match[] fixtures) {
+		this.fixtures = fixtures;
+	}
+
+}
