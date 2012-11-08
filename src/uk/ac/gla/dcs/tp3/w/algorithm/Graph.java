e51651f6db617b12376a3198c1396eff5be5ccb1
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2012-11-08 13:24:59 +0000

    Commenting Graph.java's current code.

--- src/uk/ac/gla/dcs/tp3/w/algorithm/Graph.java
+++ src/uk/ac/gla/dcs/tp3/w/algorithm/Graph.java
@@ -4,7 +4,6 @@ import uk.ac.gla.dcs.tp3.w.league.League;
 import uk.ac.gla.dcs.tp3.w.league.Match;
 import uk.ac.gla.dcs.tp3.w.league.Team;
 
-
 public class Graph {
 
 	private Vertex[] v;
@@ -12,25 +11,35 @@ public class Graph {
 	private Vertex source;
 	private Vertex sink;
 
-	public Graph(){
+	public Graph() {
 		this(null, null);
 	}
-	
-	public Graph(League l, Team t){
+
+	public Graph(League l, Team t) {
+		// Create blank vertices for source and sink.
 		source = new Vertex();
 		sink = new Vertex();
-		int teamTotal = l.getTeams().length -1;
-		int gameTotal = comb(teamTotal,2);
+		// Number of team nodes is one less than total number of teams.
+		// The team nodes do not include the team being tested for elimination.
+		int teamTotal = l.getTeams().length - 1;
+		// The r-combination of teamTotal for length 2 is the number of possible
+		// combinations for matches between the list of Teams-{t}.
+		int gameTotal = comb(teamTotal, 2);
+		// Total number of vertices is the number of teams-1 + number of match
+		// pairs + source + sink.
 		v = new Vertex[teamTotal + gameTotal + 2];
+		// Set first vertex to be source, and last vertex to be sink.
 		v[0] = source;
-		v[v.length-1] = sink;
-		for (int i=1; i<=gameTotal; i++){
+		v[v.length - 1] = sink;
+		// Create vertex for each match pair and make it adjacent from the
+		// source.
+		for (int i = 1; i <= gameTotal; i++) {
 			v[i] = new Vertex();
-			v[0].getAdjList().add(new AdjListNode(0,v[i]));
+			v[0].getAdjList().add(new AdjListNode(0, v[i]));
 		}
-		
+
 	}
-	
+
 	public Vertex[] getV() {
 		return v;
 	}
@@ -66,13 +75,15 @@ public class Graph {
 	public void setSink(Vertex sink) {
 		this.sink = sink;
 	}
-	
-	private static int fact(int s){
-		return (s<2) ? 1 : s*fact(s-1);
+
+	private static int fact(int s) {
+		// For s < 2, the factorial is 1. Otherwise, multiply s by fact(s-1)
+		return (s < 2) ? 1 : s * fact(s - 1);
 	}
-	
-	private static int comb(int n, int r){
-		return (fact(n)/(fact(r)*fact(n-r)));
+
+	private static int comb(int n, int r) {
+		// r-combination of size n is n!/r!(n-r)!
+		return (fact(n) / (fact(r) * fact(n - r)));
 	}
 
 }
