53a9da5a1f320665f9ab6accbdd72ca8027bdf0c
James Gallagher <jamesgall90@gmail.com>
2012-12-04 20:40:22 +0000

    changes to java to show Parser functionality

--- src/uk/ac/gla/dcs/tp3/w/Main.java
+++ src/uk/ac/gla/dcs/tp3/w/Main.java
@@ -3,12 +3,15 @@
  */
 package uk.ac.gla.dcs.tp3.w;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import uk.ac.gla.dcs.tp3.w.algorithm.Algorithm;
 import uk.ac.gla.dcs.tp3.w.league.Division;
 import uk.ac.gla.dcs.tp3.w.league.Match;
 import uk.ac.gla.dcs.tp3.w.league.Team;
+import uk.ac.gla.dcs.tp3.w.parser.Parser;;
 
 /**
  * @author gordon
@@ -19,15 +22,55 @@ public class Main {
 	/**
 	 * @param args
 	 */
+	
+	
+	
 	public static void main(String[] args) {
 		// Create test division.
-		Division testDivision = testUIAndAlgorithm();
+		//Division testDivision = testUIAndAlgorithm();
+		
 		// Create algorithm for division.
-		Algorithm algorithm = new Algorithm(testDivision);
+		//Algorithm algorithm = new Algorithm(testDivision);
+		
 		// Find out what teams are eliminated.
-		for (Team t : testDivision.getTeams()) {
+		//for (Team t : testDivision.getTeams()) {
+			//t.setEliminated(algorithm.isEliminated(t));
+		//}
+		
+		// import the file 
+		Parser p =null;
+		
+		try {
+		File source = new File(System.getProperty("user.dir")
+				+ "/src/uk/ac/gla/dcs/tp3/w/parser/baseballSource.txt");
+		
+		 p = new Parser(source);		
+		
+		
+		}catch (Exception e) {
+			System.out.println("Wrong filename");
+		}
+				
+		//Test to see if Parser is working
+		
+		System.out.println(p.getAmericanCentral());
+		System.out.println(p.getAmericanEast());
+		System.out.println(p.getAmericanWest());
+		
+		System.out.println(p.getNationalCentral());
+		System.out.println(p.getNationalEast());
+		System.out.println(p.getNationalWest());
+		
+		// Create algorithm for division.
+		Algorithm algorithm = new Algorithm(p.getAmericanCentral());
+		
+		
+		// Find out what teams are eliminated.
+		for (Team t : p.getAmericanCentral().getTeams()) {
 			t.setEliminated(algorithm.isEliminated(t));
 		}
+		
+		
 	}
 
 	private static Division testUIAndAlgorithm() {
