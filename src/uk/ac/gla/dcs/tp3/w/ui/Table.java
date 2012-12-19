f40833859ee3fcdffa6e38a2a896dcc263ea3f3a
gordon1992 <gordon.reid1992@hotmail.co.uk>
2012-12-19 19:27:40 +0000

    adds neccessary new files for previous commit

---
+++ src/uk/ac/gla/dcs/tp3/w/ui/Table.java
@@ -0,0 +1,28 @@
+package uk.ac.gla.dcs.tp3.w.ui;
+
+import java.util.HashMap;
+
+import javax.swing.JTable;
+
+import uk.ac.gla.dcs.tp3.w.league.Division;
+
+public class Table extends JTable {
+
+	private static final long serialVersionUID = 1L;
+	private TableModel model;
+	
+	public Table(HashMap<String, Division> divisions) {
+		super();
+		model = new TableModel(divisions);
+		this.setModel(model);
+    }
+
+	public String getCurrent() {
+		return model.getCurrent();
+	}
+	
+	public void setCurrent(String s) {
+		model.setCurrent(s);
+	}
+	
+}
